0ebdd9069dc219a872f1c4a5a64f689f158d707b
Irakli1121 <i_iremasshvili@cu.edu.ge>
2018-12-06 11:54:11 +0400

    Add Registration

---
+++ models/user.js
@@ -0,0 +1,47 @@
+let mongoose    = require("mongoose"),
+    bcrypt      = require("bcryptjs"),
+    config      = require("../config/db");
+
+//User schema
+let UserSchema = new mongoose.Schema({
+    name: {
+        type: String
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+let User = mongoose.model("User", UserSchema);
+
+module.exports = User;
+
+module.exports.getUserById = function(id, callback){
+    User.findById(id, callback);
+}
+
+module.exports.getUserByUsername = function(username, callback){
+    let query = {username: username}
+    User.findOne(query, callback);
+}
+
+module.exports.addUser = function(newUser, callback){
+    bcrypt.genSalt(10,(err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash)=>{
+            if (err){
+                throw err;
+            }
+            newUser.password = hash;
+            newUser.save(callback)
+        });
+    });
+}
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,26 @@
-let express = require('express'),
-    router = express.Router();
+let express     = require('express'),
+    router      = express.Router()
+    User        = require("../models/user"),
+    passport    = require("passport"),
+    jwt         = require("jsonwebtoken");
 
 
 //register
 router.post('/register', (req,res, next) =>{
-    res.send("<h1>REGISTER ROUTE </h1>")
+    let newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    User.addUser(newUser, (err, user) => {
+        if (err){
+            res.json({success: false, msg:"Couldn't register user"});
+        } else {
+            res.json({success: true, msg:"Registered new user"});
+        }
+    });
 });
 
 //Auth
