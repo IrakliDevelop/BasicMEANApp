a30966a0212e962f371498afd4145cce54650d63
Irakli1121 <i_iremashvili@cu.edu.ge>
2018-12-08 22:19:28 +0400

    Add login and logout functionality

--- angular-src/src/app/components/navbar/navbar.component.ts
+++ angular-src/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router, Route } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router : Router,
+    private FlashMessage : FlashMessagesService
+  ) { }
 
   ngOnInit() {
+  }
+
+  onLogoutClick(){
+    this.authService.logout();
+    this.FlashMessage.show("You have successfully logegd out",
+    {
+      cssClass: 'alert-success',
+      timeout: 3000
+    }
+    );
+    this.router.navigate(["/login"]);
+    return false;
   }
 
 }
--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -20,4 +20,25 @@ export class AuthService {
     return this.http.post('http://localhost:3000/users/register', user, {headers:headers})
       .pipe(map(response => response));
   }
+
+  authenticateUser(user):any{
+    let headers = new HttpHeaders();
+    headers.append('Content-type', 'application/json');
+    return this.http.post('http://localhost:3000/users/authenticate', user, {headers:headers})
+      .pipe(map(response => response));
+  }
+
+
+  storeUserData(token,user){
+    localStorage.setItem('id_token', token);
+    localStorage.setItem('user', JSON.stringify(user));
+    this.authToken = token;
+    this.user = user;
+  }
+
+  logout(){
+    this.authToken = null;
+    this.user = null;
+    localStorage.clear();
+  }
 }
